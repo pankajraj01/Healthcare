5477577e8aa186a89ba44f4b52d68edcdad584e9
Pankaj Rajpurohit <pankajrajpurohit01.com>
2019-06-20 12:31:15 +0530

    little changes in xmls

--- app/src/main/java/com/pankaj/healthcare/MobileNumberPageActivity.java
+++ app/src/main/java/com/pankaj/healthcare/MobileNumberPageActivity.java
@@ -1,13 +1,28 @@
 package com.pankaj.healthcare;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MobileNumberPageActivity extends AppCompatActivity {
+
+    Button btn_submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mobile_number_page);
+
+        btn_submit = findViewById(R.id.btn_submit);
+
+        btn_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MobileNumberPageActivity.this, VerifyPhoneNoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
