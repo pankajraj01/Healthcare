aae7a5856459eb9eede7846ff62830ba99a28890
Pankaj Rajpurohit <pankajrajpurohit01.com>
2019-06-20 11:56:07 +0530

    Verify phone num page UI(XML)

---
+++ app/src/main/java/com/pankaj/healthcare/VerifyPhoneNoActivity.java
@@ -0,0 +1,13 @@
+package com.pankaj.healthcare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VerifyPhoneNoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_phone_no);
+    }
+}
