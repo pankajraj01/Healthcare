9c5a6ab15b22f80a4e645086d6b66995e3f8c4cb
Pankaj Rajpurohit <pankajrajpurohit01.com>
2019-06-20 11:40:03 +0530

    Mobilenum_Page UI(xml)

--- app/src/main/java/com/pankaj/healthcare/MobileNumberPageActivity.java
+++ app/src/main/java/com/pankaj/healthcare/MobileNumberPageActivity.java
@@ -3,11 +3,11 @@ package com.pankaj.healthcare;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MobileNumberPageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_mobile_number_page);
     }
 }
