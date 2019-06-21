beefbbc2f03be563b86399426b06a7b35e210312
Pankaj Rajpurohit <pankajrajpurohit01.com>
2019-06-21 12:46:21 +0530

    Build Navigation Menu

---
+++ app/src/main/java/com/pankaj/healthcare/NavigationMenuActivity.java
@@ -0,0 +1,13 @@
+package com.pankaj.healthcare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NavigationMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_menu);
+    }
+}
